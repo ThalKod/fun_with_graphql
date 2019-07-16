55a705006b43ef5a244b8733ed60232583120d65
ThalKod <tmarcelin@thal.tech>
2019-07-16 12:26:42 -0400

    add subscription prisma

--- graphql_intermediate/src/resolvers/Subscription.js
+++ graphql_intermediate/src/resolvers/Subscription.js
@@ -1,19 +1,26 @@
 const Subscription = {
   comment: {
-    subscribe: (parents, args, ctx, info) => {
-      const { postID } = args;
-      const { db, pubsub } = ctx;
-
-      const post = db.posts.find((post) => post.id === postID );
-      if(!post) throw new Error("Post not found...");
-
-      return pubsub.asyncIterator(`comment:${postID}`); // channel name for the comments on post of postID
+    subscribe: (parents, { postID }, { prisma }, info) => {
+      return prisma.subscription.comment({
+        where: {
+          node: {
+            post: {
+              id: postID
+            }
+          }
+        }
+      }, info);
     }
   },
   post: {
-    subscribe: (parents, args, ctx, info) => {
-      const { pubsub } = ctx;
-      return pubsub.asyncIterator("post");
+    subscribe: (parents, args, { prisma }, info) => {
+      return prisma.subscription.post({
+        where: {
+          node: {
+            published: true
+          }
+        }
+      }, info)
     }
   }
 };
